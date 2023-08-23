b5d283a4517c617cfb5c07607745239963cfb3cc
Admin <sambhramps@outlook.com>
2023-08-23 15:50:44 +0530

    Add data processing functions and tests

--- client3.py
+++ client3.py
@@ -30,30 +30,49 @@ N = 500
 
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """ Returns the tuple of stock name, bid_price, ask_price, and price. """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    """ Returns the ratio of price_a and price_b. """
+    return price_a / price_b
 
 
-# Main
-if __name__ == "__main__":
+def main():
+    stock_a_prices = []
+    stock_b_prices = []
+
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
-        for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+        stock_a_quote = next(quote for quote in quotes if quote['stock'] == 'ABC')
+        stock_b_quote = next(quote for quote in quotes if quote['stock'] == 'DEF')
 
-        print("Ratio %s" % getRatio(price, price))
+        stock_a_data = getDataPoint(stock_a_quote)
+        stock_b_data = getDataPoint(stock_b_quote)
+        ratio = getRatio(stock_a_data[3], stock_b_data[3])
+
+        stock_a_prices.append(stock_a_data[3])
+        stock_b_prices.append(stock_b_data[3])
+
+        print("Stock A Info:", stock_a_data)
+        print("Stock B Info:", stock_b_data)
+        print("Ratio:", ratio)
+
+    avg_stock_a_price = sum(stock_a_prices) / len(stock_a_prices)
+    avg_stock_b_price = sum(stock_b_prices) / len(stock_b_prices)
+    final_ratio = getRatio(avg_stock_a_price, avg_stock_b_price)
+
+    print("Average Stock A Price:", avg_stock_a_price)
+    print("Average Stock B Price:", avg_stock_b_price)
+    print("Final Ratio:", final_ratio)
+
+
+if __name__ == "__main__":
+    main()
--- client_test.py
+++ client_test.py
@@ -2,24 +2,29 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+        stock_a_data = getDataPoint(quotes[0])
+        stock_b_data = getDataPoint(quotes[1])
 
+        self.assertEqual(stock_a_data, ('ABC', 120.48, 121.2, (120.48 + 121.2) / 2))
+        self.assertEqual(stock_b_data, ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2))
 
-  """ ------------ Add more unit tests ------------ """
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
 
+        stock_a_data = getDataPoint(quotes[0])
+        stock_b_data = getDataPoint(quotes[1])
 
+        self.assertEqual(stock_a_data, ('ABC', 120.48, 119.2, (120.48 + 119.2) / 2))
+        self.assertEqual(stock_b_data, ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2))
 
 if __name__ == '__main__':
     unittest.main()
